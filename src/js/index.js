be93bc3d6beb835fedaea5dac2e62f30ae76ba65
Rexkit <PTZnikko@gmail.com>
2018-06-04 16:52:18 +0300

    recipeView impl

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of app
@@ -65,6 +66,12 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -75,18 +82,18 @@ const controlRecipe = async () => {
             await state.recipe.getRecipe();
             console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
+            
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             alert('Error processing recipe!');
             console.log(error);
         }
-
-        // Calculate servings and time
-        state.recipe.calcTime();
-        state.recipe.calcServings();
-
-        // Render recipe
-        console.log(state.recipe);
-        
     }
 }
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,14 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 };
 
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 const limitRecipeTitle = (title, limit = 17) => {
     if (title.length > limit) {
         return title.substring(0, title.substring(0, limit).lastIndexOf(' ')) + ' …';
